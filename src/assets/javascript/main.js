3d8b2506cf1919b3c929e26cf662f5e78119cbcc
Liam Gray <hoxxep@gmail.com>
2017-02-11 10:59:13 +0000

    Different header, lots of tweaks

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function headerParallax() {
+  if ($(window).width() < 1024) {
+    return;
+  }
+
+  const $background = $('.hero.section .background');
+
+  $(window).scroll(event => {
+    const scroll = $(document).scrollTop();
+
+    if (scroll > $background.height()) {
+      return;
+    }
+
+    const y = scroll / 2;
+    $background.css('transform', `translate3D(0, ${y}px, 0)`);
+  });
+}
+
+try {
+  headerParallax();
+} catch (e) {
+  console.error(e);
+}
